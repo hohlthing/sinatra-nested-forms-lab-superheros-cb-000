74d59956e82a3ebfcfb09510eeecf8bcb6d5a177
michael hohl <mhohl@rocketmail.com>
2018-05-17 23:55:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
 
     post "/teams" do
       @team = Team.new(params[:team])
-      params[:team][:members].collect do |details|
+      params[:team][:hero].map do |details|
         SuperHero.new(details)
       end
 
